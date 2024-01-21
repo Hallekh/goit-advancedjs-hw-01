d0d2a107143830305dfacb5e7b85ac0ae42e639f
hallekh <lekhnovska.halyna@gmail.com>
2024-01-21 13:16:50 +0100

    task 2 vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const LS_KEY = 'videoplayer-current-time';
+
+const saveCurrentTime = throttle(function (currentTime) {
+  localStorage.setItem(LS_KEY, currentTime);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+  const currentTime = data.seconds;
+  saveCurrentTime(currentTime);
+});
+
+const savedCurrentTime = localStorage.getItem(LS_KEY);
+if (savedCurrentTime !== null) {
+  player.setCurrentTime(savedCurrentTime);
+}
