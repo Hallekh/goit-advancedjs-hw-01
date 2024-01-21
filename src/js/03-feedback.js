7003a5002180f1197b3020d94aa00dbeb790231c
hallekh <lekhnovska.halyna@gmail.com>
2024-01-21 14:51:39 +0100

    task 3 form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const LS_KEY = 'feedback-form-state';
+
+//збереження стану форми в локальному сховищі
+const saveFormState = throttle(function () {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(LS_KEY, JSON.stringify(formState));
+}, 500);
+
+// Відновлення стану форми під час завантаження сторінки
+function restoreFormState() {
+  const savedState = localStorage.getItem(LS_KEY);
+  if (savedState) {
+    const parsedState = JSON.parse(savedState);
+    emailInput.value = parsedState.email;
+    messageInput.value = parsedState.message;
+  }
+}
+
+// Отримання даних форми та очищення локального сховища при сабміті
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  // Очищення локального сховища
+  localStorage.removeItem(LS_KEY);
+
+  // Виведення даних у консоль
+  console.log('Form data submitted:', formData);
+
+  // Очищення полів форми
+  emailInput.value = '';
+  messageInput.value = '';
+});
+
+// Відстеження події input для збереження стану форми
+form.addEventListener('input', saveFormState);
+
+// Відновлення стану форми під час завантаження сторінки
+restoreFormState();
